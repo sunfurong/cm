5336c4674282defc3d98aebba8d40107e75bd02d
sunfurong <sunfurong19921124@qq.com>
2015-09-10 10:26:31 +0800

    修改一下路由的规则，防止app.js的臃肿

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ var bodyParser = require('body-parser');
 */
 var mysqlcon = require('./mysqlconnect');
 mysqlcon.handleError()
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -26,9 +26,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+// app.use('/', routes);
 app.use('/users', users);
-
+routes(app);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
