4716f2bcef906ea3876934a08ce7d2b6318e1d62
sunfurong <sunfurong19921124@qq.com>
2015-09-10 11:16:55 +0800

    引入mongodb数据库，登陆页和注册页，路由配置

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,26 @@
 var express = require('express');
 /* GET home page. */
 
-module.exports = function(app){
-	app.get('/',function(req,res){
-		 res.render('index', { title: 'Express' });
+module.exports = function(app) {
+	app.get('/', function(req, res) {
+		res.render('index', {
+			title: 'Express'
+		});
 	});
-	app.get('/sf', function (req, res) {
-  res.send('hello,world!');
-});
+	app.get('/reg', function(req, res) {
+		res.render('reg', {
+			title: '注册',
+			// user: req.session.user,
+			// success: req.flash('success').toString(),
+			 //error: req.flash('error').toString()
+		});
+	});
+	// app.post('/reg', function(req, res) {});
+	// app.get('/login', function(req, res) {
+	// 	res.render('login', {
+	// 		title: '登录'
+	// 	});
+	// });
+	// app.post('/login', function(req, res) {});
+	// app.get('/logout', function(req, res) {});
 };
