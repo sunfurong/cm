060e345dee978ba3a98825aafa9b52f75244ac24
sunfurong <sunfurong19921124@qq.com>
2015-09-11 13:56:12 +0800

    删除mongodb数据库配置，session保存到内存中，增加用户注册功能

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+//内存session
+var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+//settings配置文件
+//var settings = require('./settings');
+//引入flash
+var flash = require('connect-flash');
+
 /*
-  mysql数据库连接
-*/
-var mysqlcon = require('./mysqlconnect');
-mysqlcon.handleError()
+* session
+* */
+//var session =require('express-session');
+//var MongoStore = require('connect-mongo')(session);
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -18,6 +25,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(flash());
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -26,6 +35,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 按照上面的解释，设置 session 的可选参数
+app.use(session({
+  secret: 'mycm', // 建议使用 128 个字符的随机字符串
+  cookie: { maxAge: 60 * 1000 }
+}));
+
+//设置session
+//app.use(session({
+//  secret: settings.cookieSecret,
+//  key: settings.db,//cookie name
+//  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+//  store: new MongoStore({
+//    db: settings.db,
+//    host: settings.host,
+//    port: settings.port,
+//    auto_reconnect: true
+//  }),
+//  resave:false,
+//  saveUninitialized:true
+//}));
 // app.use('/', routes);
 app.use('/users', users);
 routes(app);
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
+var User = require('../models/user/user.js');
+var crypto = require('crypto');
 /* GET home page. */
-
+var mysqlcon = require('../models/db/mysqlconnect');
 module.exports = function(app) {
 	app.get('/', function(req, res) {
 		res.render('index', {
@@ -11,11 +13,55 @@ module.exports = function(app) {
 		res.render('reg', {
 			title: '注册',
 			// user: req.session.user,
-			// success: req.flash('success').toString(),
-			 //error: req.flash('error').toString()
+			 success: req.flash('success').toString(),
+			 error: req.flash('error').toString()
 		});
 	});
-	// app.post('/reg', function(req, res) {});
+	 app.post('/reg', function(req, res) {
+		 //mysqlcon.handleError()
+		 //var sql_string="insert into cm_user(username,password,email) value('2','2','2@qq.com')";
+		 //mysqlcon.query(sql_string,function(err,rows){
+			// console.log(err);
+		 //})
+		 var name = req.body.name,
+			 password = req.body.password,
+			 password_re = req.body['password-repeat'];
+		 if (password_re != password) {
+			 req.flash('error', '两次输入的密码不一致!');
+			 return res.redirect('/reg');
+		 }
+		 var md5 = crypto.createHash('md5'),
+			 password = md5.update(req.body.password).digest('hex');
+		 var newUser = new User({
+			 name: name,
+			 password: password,
+			 email: req.body.email
+		 });
+		 //User.get(newUser.name);
+		 User.get(newUser.name, function (err, user) {
+			 if (err) {
+				 req.flash('error', err);
+				 return res.redirect('/');
+			 }
+			 if (user) {
+				 req.flash('error', '用户已存在!');
+				 return res.redirect('/reg');
+			 }
+
+			 newUser.save(function (err, user) {
+
+				 if (err) {
+                     console.log(err);
+					 req.flash('error', err);
+					 return res.redirect('/reg');
+				 }
+                 console.log(user.username);
+				 req.session.user = user;
+				 req.flash('success', '注册成功!');
+				 res.redirect('/');
+			 });
+		 });
+	 });
 	// app.get('/login', function(req, res) {
 	// 	res.render('login', {
 	// 		title: '登录'
